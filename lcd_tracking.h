8dc476131ad79e0d07c0d9598de545ee9a2ea35d
Mohamed Raafat <44686958+mohamedar97@users.noreply.github.com>
2021-06-06 23:12:52 +0200

    initial commit
    LCD tracking logic

---
+++ lcd_tracking.h
@@ -0,0 +1,10 @@
+#ifndef LCD_TRACKING_H_INCLUDED
+#define LCD_TRACKING_H_INCLUDED
+
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+#include "lcd_cmd_data.h"
+
+void lcd_tracking1(uint32_t walked_dist);
+
+#endif
